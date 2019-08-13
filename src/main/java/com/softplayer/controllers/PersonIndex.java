43fc1f1bae28e8137b52df3654557a46b139c1d9
danielson.ramos <danielson.ramos@.cabal.com.br>
2019-08-13 11:36:11 -0300

    Criando front end

--- src/main/java/com/softplayer/controllers/PersonIndex.java
+++ src/main/java/com/softplayer/controllers/PersonIndex.java
@@ -2,16 +2,14 @@ package com.softplayer.controllers;
 
 import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.RequestMethod;
-import org.springframework.web.bind.annotation.RestController;
 
-@RestController
+@Controller
 @RequestMapping("/")
 public class PersonIndex {
 	
-    @RequestMapping(method = RequestMethod.GET)
+    @RequestMapping()
     String index(){
-        return "index";
+        return "CadastroPessoa";
     }
 
 }
