b904bbba3365cdb2ade53fb64394d55c9b854ee4
danielsonlr <danielsonlr@danielsonlr-300E4C-300E5C-300E7C>
2019-08-13 00:38:17 -0300

    Criando caminho para tela inicial

--- src/main/java/com/softplayer/config/SwaggerConfig.java
+++ src/main/java/com/softplayer/config/SwaggerConfig.java
@@ -27,12 +27,11 @@ public class SwaggerConfig extends WebMvcConfigurationSupport {
     }
     private ApiInfo metaData() {
         return new ApiInfoBuilder()
-                .title("Spring Boot REST API")
-                .description("\"Spring Boot REST API for Online Store\"")
+                .title("Softplayer")
+                .description("\"Spring Boot REST API Softplayer\"")
                 .version("1.0.0")
                 .license("Apache License Version 2.0")
-                .licenseUrl("https://www.apache.org/licenses/LICENSE-2.0\"")
-                .contact(new Contact("John Thompson", "https://springframework.guru/about/", "john@springfrmework.guru"))
+                .contact(new Contact("Danielson L Ramos", "", "danielson@gmail.com"))
                 .build();
     }
     @Override
