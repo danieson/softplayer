bba366eb2602096f2574ea910eb12938e9331fd2
danielson.ramos <danielson.ramos@.cabal.com.br>
2019-08-14 08:37:42 -0300

    Criando versão 2

--- src/main/java/com/softplayer/config/SwaggerConfig.java
+++ src/main/java/com/softplayer/config/SwaggerConfig.java
@@ -20,7 +20,7 @@ public class SwaggerConfig extends WebMvcConfigurationSupport {
     public Docket productApi() {
         return new Docket(DocumentationType.SWAGGER_2)
                 .select()
-                .apis(RequestHandlerSelectors.basePackage("com.softplayer.controllers"))
+                .apis(RequestHandlerSelectors.basePackage("com.softplayer.controllers.swagger"))
                 .paths(regex("/person.*"))
                 .build()
                 .apiInfo(metaData());
