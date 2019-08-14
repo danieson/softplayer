a29def95cb15eaf9be768c755f632ae26fcc0548
danielson.ramos <danielson.ramos@.cabal.com.br>
2019-08-14 11:13:57 -0300

    Comentando código sem uso

--- src/main/java/com/softplayer/domain/Person.java
+++ src/main/java/com/softplayer/domain/Person.java
@@ -8,16 +8,16 @@ import javax.validation.constraints.NotNull;
 @Entity
 public class Person {
     @Id
-    @NotNull(message = "CPF não pode ser nulo")
-	@NotEmpty(message = "CPF não pode ser nulo")
+//    @NotNull(message = "CPF não pode ser nulo")
+//	@NotEmpty(message = "CPF não pode ser nulo")
 	private String cpf;
 	private String nome;
 	private String sexo;
 	private String email;
 	private String naturalidade;
 	private String nacionalidade;
-    @NotNull(message = "Data Nascimento não pode ser nulo")
-	@NotEmpty(message = "Data Nascimento não pode ser nulo")
+//    @NotNull(message = "Data Nascimento não pode ser nulo")
+//	@NotEmpty(message = "Data Nascimento não pode ser nulo")
 	private String dataNascimento;
 	
 	public String getCpf() {
