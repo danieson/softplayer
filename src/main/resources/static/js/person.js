e6fd5a2fbf79b89cd9b2c0dc49fbe56c2157c5d7
danielson.ramos <danielson.ramos@.cabal.com.br>
2019-08-14 11:09:02 -0300

    Corrigindo bug

--- src/main/resources/static/js/person.js
+++ src/main/resources/static/js/person.js
@@ -6,11 +6,11 @@ $('#confirmacaoExclusaoModal').on('show.bs.modal', function(event) {
 	
 	var modal = $(this);
 	var form = modal.find('form');
-	var action = form.attr('action');
+	var action = form.data('url-base');
 	if (!action.endsWith('/')) {
 		action += '/';
 	}
 	form.attr('action', action + codigoTitulo);
 	
-	modal.find('.modal-body span').html('Tem certeza que deseja apagar os dados da pessoa? <strong>' + descricaoTitulo + '</strong>?');
+	modal.find('.modal-body span').html('Tem certeza que deseja excluir o título <strong>' + descricaoTitulo + '</strong>?');
 });
