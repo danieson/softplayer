27d584aff048672684ab715de025d9e44407d857
danielson.ramos <danielson.ramos@.cabal.com.br>
2019-08-15 12:41:01 -0300

    Adicionando validação basic

--- src/main/java/com/softplayer/config/SpringSecConfig.java
+++ src/main/java/com/softplayer/config/SpringSecConfig.java
@@ -1,40 +1,27 @@
 package com.softplayer.config;
 
 import org.springframework.context.annotation.Configuration;
+import org.springframework.security.config.annotation.authentication.builders.AuthenticationManagerBuilder;
 import org.springframework.security.config.annotation.web.builders.HttpSecurity;
 import org.springframework.security.config.annotation.web.configuration.WebSecurityConfigurerAdapter;
+import org.springframework.security.config.http.SessionCreationPolicy;
 
 @Configuration
 public class SpringSecConfig extends WebSecurityConfigurerAdapter {
 
-    @Override
-    protected void configure(HttpSecurity httpSecurity) throws Exception {
-        httpSecurity.authorizeRequests().antMatchers("/","/swagger-resources").permitAll();
-        httpSecurity.csrf().disable();
-        httpSecurity.headers().frameOptions().disable();
-    }
-
-	//AUTENTICAÇÃO BÁSICA
-//    @Override
-//    protected void configure(HttpSecurity http) throws Exception {
-//      http
-//          .authorizeRequests()
-//              // Para qualquer requisição (anyRequest) é preciso estar 
-//              // autenticado (authenticated).
-//              .anyRequest().authenticated()
-//          .and()
-//          .httpBasic();
-//    }
-//
-//    
-//    @Override
-//    public void configure(AuthenticationManagerBuilder builder) throws Exception {
-//      builder
-//          .inMemoryAuthentication()
-//          .withUser("garrincha").password("123")
-//              .roles("USER")
-//          .and()
-//          .withUser("zico").password("123")
-//              .roles("USER");
-//    }
+	@Override
+	protected void configure(AuthenticationManagerBuilder auth) throws Exception {
+		auth.inMemoryAuthentication()
+			.withUser("admin").password("{noop}admin").roles("ROLE");
+	}
+	
+	@Override
+	protected void configure(HttpSecurity http) throws Exception {
+		http.authorizeRequests()
+				.anyRequest().authenticated()
+				.and()
+			.httpBasic().and()
+			.sessionManagement().sessionCreationPolicy(SessionCreationPolicy.STATELESS).and()
+			.csrf().disable();
+	}
 }
